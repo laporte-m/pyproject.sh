4e9080848b896fa5cdd0a6fbb393a16de7eab09a
Matt Laporte <matt.l.laporte@gmail.com>
2016-05-24 17:20:44 -0400

    Removed obvious comments, slight improvement to documentation including usage

--- pyproject.sh
+++ pyproject.sh
@@ -1,56 +1,45 @@
 #!/bin/bash
 
 # Python project setup
+#
+# Creates a local Python 3 virtual environment with pyvenv
+#
+#   $ ./pyproject.sh myproject
+#
 
+# Assign your user name to USER, or specify alternate DEV directory for projects
 USER="mll"
 DEV="/home/$USER/dev"
 
-# location of this script
+# Location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 NAME=$1
 PRJDIR="$DEV/$NAME"
 LIC=${2:-GPLv3}  # use GPLv3 by default
-COMMIT="Project setup"
+COMMIT="Project structure (pyproject.sh)"
 
 mkdir -p "$PRJDIR/"
 cd "$PRJDIR"
 
-# Setup main project package
+pyvenv venv
+
+touch "README.md"
+cp "$DIR/template/$LIC.txt" "$PRJDIR/LICENSE.txt"
 mkdir "$NAME/"
 touch "$NAME/__init__.py"
 
-# Setup tests package
 mkdir "tests/"
 touch "tests/__init__.py"
 echo "import $NAME" >> "tests/test_$NAME.py"
 
-# Create documentation 
 mkdir "docs/"
-cp "$DIR/template/.gitignore" "$PRJDIR"
-cp "$DIR/template/$LIC.txt" "$PRJDIR/LICENSE.txt"
-touch "$PRJDIR/README.rst"
 
-# Copy setup.py and replace name and license information
 cp "$DIR/template/setup.py" "$PRJDIR"
 sed -i 's/NAME/$NAME/g' setup.py
 sed -i 's/LIC/$LIC/g' setup.py
 
-# Initialize git repository and commit project structure
 git init
+cp "$DIR/template/.gitignore" "$PRJDIR"
 git add "*"
 git commit -m "$COMMIT"
-
-# Create project virtual environment
-pyvenv venv
-
-
-
-
-
-
-
-
-
-
-
